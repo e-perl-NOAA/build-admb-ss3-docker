aa100cc422dd49170be601a05c454da5c232e2f1
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-03-22 11:01:42 -0400

    Create install_r_packages.R

---
+++ install_r_packages.R
@@ -0,0 +1,26 @@
+# Install R packages for hake assessment docker image
+
+install.packages("remotes")
+library(remotes)
+library(purrr)
+library(usethis)
+
+# These are the packages on GitHub, in alphabetical order
+github_pac_lst <- c("r4ss/r4ss",
+                    "ss3sim/ss3sim",
+                    "PIFSCstockassessments/ss3diags")
+
+walk(github_pac_lst, \(pkg){
+  install_github(pkg)
+})
+
+# These are packages on CRAN. Alphabetical order, each line has the same
+# starting letter for each package (new line for new starting letter)
+pac_lst <- c(
+  "corrplot", "cowplot", "data.table", "data.tree", "date",
+  "future", "furrr", "ggpubr", "glue", "grDevices", "grid", "gridExtra", 
+  "gridGraphics", "gtable", "here", "knitr", "magick", "parallelly", 
+  "RColorBrewer", "readr", "scales", "sf", "tidyverse",
+  "tidyselect", "tictoc", "tools", "utils", "with")
+
+install.packages(pac_lst)
